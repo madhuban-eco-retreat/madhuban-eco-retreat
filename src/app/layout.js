479711318835374e69c7bb889f20383f1f66abab
ajay <ajaykushwaha626162@gmail.com>
2026-01-20 12:29:43 +0530

    FIX GOOGLE tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,12 +47,11 @@ export default function RootLayout({ children }) {
         </Script>
 
         <Script id="googletagmanager" strategy="beforeInteractive">
-          {`
-           <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MKKPGJ72');</script>`}
+          })(window,document,'script','dataLayer','GTM-MKKPGJ72');`}
         </Script>
       </head>
 
