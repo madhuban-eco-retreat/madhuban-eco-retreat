9012d2801d17adb2daa3ec3f2c083b3f23a993d9
ajay <ajaykushwaha626162@gmail.com>
2026-02-03 19:04:01 +0530

    fix backend URL

--- src/services/blog/blogServices.js
+++ src/services/blog/blogServices.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const serverUrl = "https://admin-backend-dev.onrender.com";
+const serverUrl = "https://admin-backend-prod-xvpb.onrender.com";
 
 export const getAllBlogs = async (pageCount, limit) => {
   try {
     const res = await axios.get(
-      `${serverUrl}/api/blogs/all/madhubhan?type=blog&status=Published&page=${pageCount}&limit=${limit}`
+      `${serverUrl}/api/blogs/all/madhubhan?type=blog&status=Published&page=${pageCount}&limit=${limit}`,
     );
     return res;
   } catch (err) {
